704f87761356368ed779486d4f597ec620b4de76
K <armandpountie@MacBook-Pro-de-Armand.local>
2023-03-07 04:28:15 +0100

    create new services (UserDetailsImp and UserDetailsServiceImp), added roles attribute in User

--- backend/lib-app/src/main/java/com/mongo/libapp/models/User.java
+++ backend/lib-app/src/main/java/com/mongo/libapp/models/User.java
@@ -1,5 +1,8 @@
 package com.mongo.libapp.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,6 +13,7 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
@@ -50,6 +54,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 
 }
---
+++ backend/lib-app/src/main/java/com/mongo/libapp/services/UserDetailsServiceImp.java
@@ -0,0 +1,25 @@
+package com.mongo.libapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mongo.libapp.models.User;
+import com.mongo.libapp.repositories.UserRepository;
+
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImp.build(user);
+    }
+}
